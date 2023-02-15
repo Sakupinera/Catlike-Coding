6c5bbd1162a8c97cc695a4c06e7c70083cc13ebe
Sakupinera <Sakupinera@gmail.com>
2023-02-15 15:14:32 +0800

    【Catlike-Coding】完成Movement章节-9.Swimming。

--- Assets/Scripts/Movement/StableFloatingRigidbody.cs
+++ Assets/Scripts/Movement/StableFloatingRigidbody.cs
@@ -1,92 +1,12 @@
-#define Swimming
 using UnityEngine;
 
-#if CustomGravity
 namespace Assets.Scripts.Movement
 {
     /// <summary>
-    /// 自定义刚体组件
+    /// 稳定的漂浮物刚体组件
     /// </summary>
     [RequireComponent(typeof(Rigidbody))]
-    public class CustomGravityRigidbody : MonoBehaviour
-    {
-#region 方法
-
-        /// <summary>
-        /// 初始化
-        /// </summary>
-        private void Awake()
-        {
-            m_rigidbody = GetComponent<Rigidbody>();
-            m_rigidbody.useGravity = false;
-        }
-
-        /// <summary>
-        /// 更新重力
-        /// </summary>
-        private void FixedUpdate()
-        {
-            if (m_floatToSleep)
-            {
-                // 检测刚体是否处于睡眠状态
-                if (m_rigidbody.IsSleeping())
-                {
-                    m_floatDelay = 0f;
-                    GetComponent<Renderer>().material.SetColor("_Color", Color.grey);
-                    return;
-                }
-
-                // 如果刚体速度非常低，我们就认为刚体静止了
-                if (m_rigidbody.velocity.sqrMagnitude < 0.0001f)
-                {
-                    m_floatDelay += Time.deltaTime;
-                    GetComponent<Renderer>().material.SetColor("_Color", Color.yellow);
-                    if (m_floatDelay >= 1f)
-                        return;
-                }
-                else
-                {
-                    m_floatDelay = 0f;
-                    GetComponent<Renderer>().material.SetColor("_Color", Color.red);
-                }
-            }
-
-            m_rigidbody.AddForce(CustomGravity.GetGravity(m_rigidbody.position), ForceMode.Acceleration);
-        }
-
-#endregion
-
-#region 依赖的字段
-
-        /// <summary>
-        /// 刚体组件
-        /// </summary>
-        private Rigidbody m_rigidbody;
-
-        /// <summary>
-        /// 停止检测延时
-        /// </summary>
-        private float m_floatDelay;
-
-        /// <summary>
-        /// 物体是否允许进入睡眠状态
-        /// </summary>
-        [SerializeField]
-        private bool m_floatToSleep = false;
-
-#endregion
-    }
-}
-#endif
-
-#if Swimming
-namespace Assets.Scripts.Movement
-{
-    /// <summary>
-    /// 自定义刚体组件
-    /// </summary>
-    [RequireComponent(typeof(Rigidbody))]
-    public class CustomGravityRigidbody : MonoBehaviour
+    public class StableFloatingRigidbody : MonoBehaviour
     {
         #region 方法
 
@@ -97,6 +17,7 @@ namespace Assets.Scripts.Movement
         {
             m_rigidbody = GetComponent<Rigidbody>();
             m_rigidbody.useGravity = false;
+            m_submergence = new float[m_buoyancyOffsets.Length];
         }
 
         /// <summary>
@@ -113,7 +34,6 @@ namespace Assets.Scripts.Movement
                     GetComponent<Renderer>().material.SetColor("_Color", Color.grey);
                     return;
                 }
-
                 // 如果刚体速度非常低，我们就认为刚体静止了
                 if (m_rigidbody.velocity.sqrMagnitude < 0.0001f)
                 {
@@ -130,16 +50,21 @@ namespace Assets.Scripts.Movement
             }
 
             m_gravity = CustomGravity.GetGravity(m_rigidbody.position);
-            // 计算物体在水中受到的力
-            if (m_submergence > 0f)
+            float dragFactor = m_waterDrag * Time.deltaTime / m_buoyancyOffsets.Length;
+            float buoyancyFactor = -m_buoyancy / m_buoyancyOffsets.Length;
+            for (int i = 0; i < m_buoyancyOffsets.Length; i++)
             {
-                float drag = Mathf.Max(0f, 1f - m_waterDrag * m_submergence * Time.deltaTime);
-                m_rigidbody.velocity *= drag;
-                m_rigidbody.angularVelocity *= drag;
-                // 使物体朝着特定方向漂浮
-                m_rigidbody.AddForceAtPosition(m_gravity * -(m_buoyancy * m_submergence), 
-                    transform.TransformPoint(m_buoyancyOffset), ForceMode.Acceleration);
-                m_submergence = 0f;
+                // 计算物体在水中受到的力
+                if (m_submergence[i] > 0f)
+                {
+                    float drag = Mathf.Max(0f, 1f - dragFactor * m_submergence[i]);
+                    m_rigidbody.velocity *= drag;
+                    m_rigidbody.angularVelocity *= drag;
+                    // 使物体朝着特定方向漂浮
+                    m_rigidbody.AddForceAtPosition(m_gravity * (buoyancyFactor * m_submergence[i]),
+                        transform.TransformPoint(m_buoyancyOffsets[i]), ForceMode.Acceleration);
+                    m_submergence[i] = 0f;
+                }
             }
             m_rigidbody.AddForce(m_gravity, ForceMode.Acceleration);
         }
@@ -173,15 +98,20 @@ namespace Assets.Scripts.Movement
         /// </summary>
         private void EvaluateSubmergence()
         {
-            Vector3 upAxis = -m_gravity.normalized;
-            if (Physics.Raycast(m_rigidbody.position + upAxis * m_submergenceOffset, -upAxis, out RaycastHit hit,
-                    m_submergenceRange + 1f, m_waterMask, QueryTriggerInteraction.Collide))
+            Vector3 down = m_gravity.normalized;
+            Vector3 offset = down * -m_submergenceOffset;
+            for (int i = 0; i < m_buoyancyOffsets.Length; i++)
             {
-                m_submergence = 1f - hit.distance / m_submergenceRange;
-            }
-            else
-            {
-                m_submergence = 1f;
+                Vector3 p = offset + transform.TransformPoint(m_buoyancyOffsets[i]);
+                if (Physics.Raycast(p, down, out RaycastHit hit,
+                        m_submergenceRange + 1f, m_waterMask, QueryTriggerInteraction.Collide))
+                {
+                    m_submergence[i] = 1f - hit.distance / m_submergenceRange;
+                }
+                else if (!m_safeFloating || Physics.CheckSphere(p, 0.01f, m_waterMask, QueryTriggerInteraction.Collide))
+                {
+                    m_submergence[i] = 1f;
+                }
             }
         }
 
@@ -204,6 +134,12 @@ namespace Assets.Scripts.Movement
         /// </summary>
         [SerializeField]
         private bool m_floatToSleep = false;
+
+        /// <summary>
+        /// 是否需要额外检查物体是否完全浸入水中
+        /// </summary>
+        [SerializeField]
+        private bool m_safeFloating = false;
 
         /// <summary>
         /// 中心点距离最上方的偏移距离
@@ -238,7 +174,7 @@ namespace Assets.Scripts.Movement
         /// <summary>
         /// 浸水程度
         /// </summary>
-        private float m_submergence;
+        private float[] m_submergence;
 
         /// <summary>
         /// 重力
@@ -246,12 +182,11 @@ namespace Assets.Scripts.Movement
         private Vector3 m_gravity;
 
         /// <summary>
-        /// 浮力偏移向量
+        /// 浮力偏移量数组
         /// </summary>
         [SerializeField]
-        private Vector3 m_buoyancyOffset = Vector3.zero;
+        private Vector3[] m_buoyancyOffsets = default;
 
         #endregion
     }
 }
-#endif
